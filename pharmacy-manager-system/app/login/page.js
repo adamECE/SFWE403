9cdbe568ca48f288e676b66f203c0833aa22f957
gatoAndres5 <andgalv@arizona.edu>
2023-09-22 13:49:46 -0700

    Added login display

--- pharmacy-manager-system/app/login/page.js
+++ pharmacy-manager-system/app/login/page.js
@@ -2,9 +2,22 @@
 
 export default function Login() {
     return (
-        <div>
+        <><div>
             <h1 className="test">I am the login page</h1>
         </div>
+        <div className="form-container">
+        <input
+          type="text"
+          placeholder="Username"
+          className="input"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="input"
+        />
+        <button className="button">Login</button>
+      </div></>
     )
   }
   
