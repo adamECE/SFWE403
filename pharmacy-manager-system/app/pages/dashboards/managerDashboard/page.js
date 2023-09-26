dd7dcd83fe16d7921f301fce68b40fefbb58d8ed
JAntonio <judithantonio@email.arizona.edu>
2023-09-25 18:37:06 -0700

    create accounts option 2

--- pharmacy-manager-system/app/pages/dashboards/managerDashboard/page.js
+++ pharmacy-manager-system/app/pages/dashboards/managerDashboard/page.js
@@ -1,10 +1,7 @@
-
-
 export default function ManagerDashboard() {
-    return (
-      <div>
-        <h1>I am the ManagerDashboard page</h1>
-      </div>
-    )
-  }
-  
+  return (
+    <div>
+      <h1> I am the ManagerDashboard page </h1>{" "}
+    </div>
+  );
+}
