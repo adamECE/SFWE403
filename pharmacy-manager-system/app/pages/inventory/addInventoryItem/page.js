3af45f794ca7b545d0d4a4d039824af72bdeb7af
JAntonio <judithantonio@email.arizona.edu>
2023-10-13 03:42:34 -0700

    connecting be-fe for place order, update order and add inven item

--- pharmacy-manager-system/app/pages/inventory/addInventoryItem/page.js
+++ pharmacy-manager-system/app/pages/inventory/addInventoryItem/page.js
@@ -1,76 +1,112 @@
-'use client'
+"use client";
 import { useState, useEffect } from "react";
-import { useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function AddToInventoryPage() {
-    const [formData,          setFormData]          = useState(); // Do we need this? 
-    const [inventoryItemInfo, setInventoryItemInfo] = useState(
-        {name:'',
-        description:'',
-        manufacturer:'',
-        price: '',
-        location: '',
-        });
-    const [selectedOption, setSelectedOption] = useState('prescription');
-    const [warningMsg, setWarningMsg] = useState('');
+  const initialState = {
+    name: "",
+    description: "",
+    manufacturer: "",
+    price: "",
+    location: "",
+  };
+  const [inventoryItemInfo, setInventoryItemInfo] = useState(initialState);
+  const [selectedOption, setSelectedOption] = useState("prescription");
+  const [warningMsg, setWarningMsg] = useState("");
 
+  const router = useRouter();
 
-    const router = useRouter();
+  const handleChange = (e) => {
+    setInventoryItemInfo({
+      ...inventoryItemInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleChange = (e) => { 
-        setInventoryItemInfo({ ...inventoryItemInfo, [e.target.name]: e.target.value });
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault(); 
+    if (inventoryItemInfo.price <= 0) {
+      setWarningMsg("Price should not be zero");
+    } else {
+      setWarningMsg("");
+      // TODO: Connect FE and Backend
 
-        if (!(/^-?\d+$/.test(inventoryItemInfo.price))) {
-            setWarningMsg('Please submit a valid price');
+      try {
+        const token = localStorage.getItem("token");
+
+        const response = await fetch(
+          "http://127.0.0.1:3030/pharmacy-0x2/api/inventory/add-item",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              // Include the bearer token in the Authorization header
+              Authorization: `Bearer ${token}`,
+            },
+
+            body: JSON.stringify({
+              name: inventoryItemInfo.name,
+              description: inventoryItemInfo.description,
+              category: selectedOption,
+              price: inventoryItemInfo.price,
+              manufacturer: inventoryItemInfo.manufacturer,
+              location: inventoryItemInfo.location,
+            }),
+          }
+        );
+
+        if (response.ok) {
+          setInventoryItemInfo(initialState);
+          const responseText = await response.text();
+          alert(JSON.parse(responseText).message);
+          router.refresh();
         } else {
-            setWarningMsg('');
-            // TODO: Connect FE and Backend
-            // all field inputs can be found in inventoryItemInfo
-            // except for the category which is found in selectedOption
-            console.log(inventoryItemInfo)
-            console.log(selectedOption)
+          setInventoryItemInfo(initialState);
+          const errorText = await response.text();
+          alert(JSON.parse(errorText).error);
         }
-
-
+      } catch (error) {
+        console.error("error:", error);
+      }
+      // all field inputs can be found in inventoryItemInfo
+      // except for the category which is found in selectedOption
+      console.log(inventoryItemInfo);
+      console.log(selectedOption);
     }
+  };
 
-    const handleBackToInv = (e) => { 
-        router.push('/pages/inventory')
-    };
+  const handleBackToInv = (e) => {
+    router.push("/pages/inventory");
+  };
 
-    // All styles 
-    const blockStyle = "m-5 p-5 flex flex-col justify-center items-center";
-    const centerStyle = "text-center text-white";
-    const inputStyle =
-    "shadow appearance-none border rounded w-full py-2" + 
-    "px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline";
-    const submitButtonStyle =
-    "bgCor hover:bg-cyan-600  border rounded w-full my-5 py-2" + 
-    "text-white appearance-none focus:outline-none focus:shadow-outline";
-    const labelSyle = "block text-white text-sm font-bold mb-2";
-    const formStyle = "max-w-[800px] w-full mx-auto bg-transparent p-4 rounded" + 
+  // All styles
+  const blockStyle = "m-5  p-5 flex flex-col justify-center items-center";
+  const centerStyle = "text-center text-white";
+  const inputStyle =
+    "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline";
+  const submitButtonStyle =
+    "bgCor border rounded w-full my-5 py-2  text-white appearance-none focus:outline-none focus:shadow-outline";
+  const labelSyle = "block text-white text-sm font-bold mb-2";
+  const formStyle =
+    "max-w-[800px] w-full mx-auto bg-transparent p-4 rounded" +
     "border border-blue-500";
 
-    return (
-        <div>
-        <button className="top-5 left-0 m-2 px-4 py-2 bg-blue-500 text-white rounded absolute"
-            onClick={handleBackToInv}>
-            Back To Inventory
-        </button>
-        <h2 className={centerStyle}>Add Invenotry Item</h2>
-            <div className={blockStyle}>
-
-        <form onSubmit={handleSubmit}
-            className={formStyle}>
-             <h3> Inventory Info </h3>
-          <hr className="mb-2" />
+  return (
+    <div>
+      <button
+        className="top-5 left-0 m-2 px-4 py-2 bgCor text-white rounded absolute"
+        onClick={handleBackToInv}
+      >
+        Back To Inventory{" "}
+      </button>{" "}
+      <h2 className={centerStyle}> Add Inventory Item </h2>{" "}
+      <div className={blockStyle}>
+        <form onSubmit={handleSubmit} className={formStyle}>
+          <h3> Inventory Info </h3> <hr className="mb-2" />
           <div className="w-full  md:flex flex-1">
             <div className="w-full mx-2">
-              <label className={labelSyle}>Name</label>
+              <label className={labelSyle}>Medication Name </label>{" "}
               <input
                 type="text"
                 placeholder="name"
@@ -80,12 +116,10 @@ export default function AddToInventoryPage() {
                 value={inventoryItemInfo.name}
                 onChange={handleChange}
                 required={true}
-              />
-            </div>
-          </div>
-          <div className="w-full  md:flex flex-1">
+              />{" "}
+            </div>{" "}
             <div className="w-full mx-2">
-              <label className={labelSyle}>Description</label>
+              <label className={labelSyle}> Description </label>{" "}
               <input
                 type="text"
                 placeholder="description"
@@ -95,35 +129,34 @@ export default function AddToInventoryPage() {
                 value={inventoryItemInfo.description}
                 onChange={handleChange}
                 required={true}
-              />
-            </div>
-          </div>
+              />{" "}
+            </div>{" "}
+          </div>{" "}
+          <div className="w-full  md:flex flex-1"></div>{" "}
           <div className="w-full  md:flex flex-1">
             <div className="w-full mx-2 text-white">
-              <label className={labelSyle}>category</label>
-              <input 
-                type="radio" 
-                name="prescription" 
-                value="prescription" 
+              <label className={labelSyle}> category </label>{" "}
+              <input
+                type="radio"
+                name="prescription"
+                value="prescription"
                 className="mx-5 px-5"
-                checked={selectedOption === 'prescription'}
+                checked={selectedOption === "prescription"}
                 onChange={(e) => setSelectedOption(e.target.value)}
-                /> 
-                Prescription
-              <input 
-                type="radio" 
-                name="over-the-counter" 
-                value="over-the-counter" 
+              />
+              Prescription{" "}
+              <input
+                type="radio"
+                name="over-the-counter"
+                value="over-the-counter"
                 className="mx-5 px-5"
-                checked={selectedOption === 'over-the-counter'}
+                checked={selectedOption === "over-the-counter"}
                 onChange={(e) => setSelectedOption(e.target.value)}
-                /> 
-                Over the Counter
-            </div>
-          </div>
-          <div className="w-full  md:flex flex-1">
+              />
+              Over the Counter{" "}
+            </div>{" "}
             <div className="w-full mx-2">
-              <label className={labelSyle}>Manufacturer</label>
+              <label className={labelSyle}> Manufacturer </label>{" "}
               <input
                 type="text"
                 placeholder="manufacturer"
@@ -133,27 +166,27 @@ export default function AddToInventoryPage() {
                 value={inventoryItemInfo.manufacturer}
                 onChange={handleChange}
                 required={true}
-              />
-            </div>
-          </div>
+              />{" "}
+            </div>{" "}
+          </div>{" "}
           <div className="w-full  md:flex flex-1">
             <div className="w-full mx-2">
-              <label className={labelSyle}>Price</label>
+              <label className={labelSyle}> Price </label>{" "}
               <input
-                type="text"
+                type="number"
                 placeholder="price"
                 className={inputStyle}
                 id="price"
                 name="price"
+                min="0"
+                step="0.01"
                 value={inventoryItemInfo.price}
                 onChange={handleChange}
                 required={true}
-              />
-            </div>
-          </div>
-          <div className="w-full  md:flex flex-1">
+              />{" "}
+            </div>{" "}
             <div className="w-full mx-2">
-              <label className={labelSyle}>Location</label>
+              <label className={labelSyle}> Location </label>{" "}
               <input
                 type="text"
                 placeholder="location"
@@ -163,17 +196,16 @@ export default function AddToInventoryPage() {
                 value={inventoryItemInfo.location}
                 onChange={handleChange}
                 required={true}
-              />
-            </div>
-          </div>
-          {true && <div className="mx-2 my-2 text-red-500">{warningMsg}</div>}
+              />{" "}
+            </div>{" "}
+          </div>{" "}
+          <div className="w-full  md:flex flex-1"></div>{" "}
+          {true && <div className="mx-2 my-2 text-red-500"> {warningMsg} </div>}{" "}
           <button className={submitButtonStyle} type="submit">
-              Place Order
+            Place Order{" "}
           </button>
-          
         </form>
-        
-        </div>
-      </div>
-    );
-  }
+      </div>{" "}
+    </div>
+  );
+}
--- pharmacy-manager-system/app/pages/orders/OrdersRow.js
+++ pharmacy-manager-system/app/pages/orders/OrdersRow.js
@@ -1,48 +1,49 @@
-
-
 export default function OrdersRow({
-    popupWindow, 
-    setPopupWindow,
-    setPopupWindowContent, 
-    medicationID,
-    name,
-    quantity,
-    supplier,
-    orderDate,
-    receptionDate,
-    status
-  }) {
-  
-    const handleRowOnClick = (e) => {
-      e.preventDefault(); 
-  
-      setPopupWindow(true); 
-  
-      setPopupWindowContent({
-        medicationID: medicationID, 
-        quantity: quantity,
-        supplier: supplier,
-        orderDate: orderDate,
-        receptionDate: receptionDate,
-        status: status,
-      }) 
-  
-    } 
-  
-    return (
-      <tr className={popupWindow ? 
-        "border-b dark:border-neutral-500 trBg" : 
-        "border-b dark:border-neutral-500 trBg hover:scale-105" } 
-        onClick={handleRowOnClick}>
-        <td className="px-6 py-4">  {name}      </td>
-        <td className="px-6 py-4">  {quantity}      </td>
-        <td className="px-6 py-4">  {supplier}      </td>
-        {/* Assuming these two should will be updated as date strings but not sure */}
-        <td className="px-6 py-4">  {orderDate}     </td>
-        <td className="px-6 py-4">  {receptionDate} </td>
-        <td className="px-6 py-4">  {status}        </td>
+  orderID,
+  popupWindow,
+  setPopupWindow,
+  setPopupWindowContent,
+  medicationID,
+  quantity,
+  supplier,
+  medicationName,
+  orderDate,
+  receptionDate,
+  status,
+}) {
+  const handleRowOnClick = (e) => {
+    e.preventDefault();
 
-      </tr>
-    );
-  }
-  
+    setPopupWindow(true);
+
+    setPopupWindowContent({
+      orderID: orderID,
+      medicationID: medicationID,
+      medicationName: medicationName,
+      quantity: quantity,
+      supplier: supplier,
+      orderDate: orderDate,
+      receptionDate: receptionDate,
+      status: status,
+    });
+  };
+
+  return (
+    <tr
+      className={
+        popupWindow
+          ? "border-b dark:border-neutral-500 trBg"
+          : "border-b dark:border-neutral-500 trBg hover:scale-105 cursor-pointer"
+      }
+      onClick={handleRowOnClick}
+    >
+      <td className="px-6 py-4"> {medicationName} </td>{" "}
+      <td className="px-6 py-4"> {quantity} </td>{" "}
+      <td className="px-6 py-4"> {supplier} </td>{" "}
+      {/* Assuming these two should will be updated as date strings but not sure */}{" "}
+      <td className="px-6 py-4"> {orderDate} </td>{" "}
+      <td className="px-6 py-4"> {receptionDate} </td>{" "}
+      <td className="px-6 py-4"> {status} </td>{" "}
+    </tr>
+  );
+}
