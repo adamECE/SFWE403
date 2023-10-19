d3c88b1014baedffbee9f20cf627ca777d029aa2
kimsh <kimshawn02@icloud.com>
2023-10-19 15:13:14 -0700

    stuff

---
+++ pharmacy-manager-system/app/pages/dashboards/pharmacistDashboard/viewPatientMedicineHistory/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+import viewPatientMedicineHistoryForm from "/app/pages/components/viewPatientMedicineHistoryForm"
+export default function ViewPatientMedicineHistory() {
+  return (
+
+    <ViewPatientMedicineHistoryForm/>
+
+  );
+}
