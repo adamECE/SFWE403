4eeafdfcb78a288f430dea741c1c87b6a4b220e6
JAntonio <judithantonio@email.arizona.edu>
2023-10-20 11:40:17 -0700

    linked BE and FE

--- pharmacy-manager-system/app/pages/viewUsers/PopupUsers.js
+++ pharmacy-manager-system/app/pages/viewUsers/PopupUsers.js
@@ -1,59 +1,58 @@
 "use client";
-import React, { useState, useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
+import Swal from "sweetalert2";
 export default function PopupUsers({
-    popupWindowContent,
-    setPopupWindow,
-    usersUpdated,
-    setUsersUpdated,
+  popupWindowContent,
+  setPopupWindow,
+  usersUpdated,
+  setUsersUpdated,
 }) {
-  const [confirmDeleteWindow, setConfirmDeleteWindow] = useState(false);
-  const [itemNotExpired, setItemNotExpired] = useState(false);
-
   const handleCloseModalBtn = (e) => {
     e.preventDefault();
     setPopupWindow(false);
     // Make sure everything else is closed
-    setItemNotExpired(false);
-    setConfirmDeleteWindow(false);
   };
 
   const handleUnlockAccount = async (e) => {
     e.preventDefault();
+    Swal.fire({
+      title: " Are you sure you want unlock this account?",
+      showDenyButton: true,
+      confirmButtonText: "YES",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const token = localStorage.getItem("token");
 
-    const token = localStorage.getItem("token"); // Get auth token from localStorage
+          const response = await fetch(
+            "http://127.0.0.1:3030/pharmacy-0x2/api/unlock-account",
+            {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
 
-    if (!confirmDeleteWindow) {
-      setConfirmDeleteWindow(true);
-    } else {
-      // Delete item from prescription after user confirms deletion
-      await fetch("http://127.0.0.1:3030/pharmacy-0x2/api/unlock-account", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json", // Specify the content type as JSON
-          Authorization: `Bearer ${token}`, // Include bearer token in the Autho header
-        },
-        body: 
-        JSON.stringify({
-          "email": "japinto.ja@gmail.com"
-        })
-        
-      })
-        .then((res) => {
-          if (res.status === 401) {
-            // Let the user know the item is not expired
-            //setItemNotExpired(true);
-            setConfirmDeleteWindow(false);
-          } else {
+              body: JSON.stringify({
+                email: popupWindowContent.email,
+              }),
+            }
+          );
+          if (response.ok) {
+            const responseText = await response.text();
             setUsersUpdated(!usersUpdated);
             setPopupWindow(false);
-            console.log("Here");
+            Swal.fire(`${JSON.parse(responseText).message}`, "", "success");
+          } else {
+            const errorText = await response.text();
+            Swal.fire(`${JSON.parse(errorText).error}`, "", "error");
           }
-        })
-        .catch((error) => {
-          console.error("Delete error:", error);
-        });
-    }
+        } catch (error) {
+          console.error("error:", error);
+        }
+      } else if (result.isDenied) {
+      }
+    });
   };
 
   return (
@@ -66,46 +65,43 @@ export default function PopupUsers({
           className="top-0 right-0 m-2 px-4 py-2 bg-blue-500 text-white rounded absolute"
           onClick={handleCloseModalBtn}
         >
-          &times;
-        </button>
-        <h3 className="text-black">{popupWindowContent.name}</h3>
-        <div className="font-bold"> User:</div>
+          &times;{" "}
+        </button>{" "}
+        <h3 className="text-black"> {popupWindowContent.name} </h3>{" "}
+        <div className="font-bold"> User: </div>{" "}
         <div className="border-2 px-4 py-2">
-          {popupWindowContent.firstName} {popupWindowContent.lastName}
-        </div>
+          {" "}
+          {popupWindowContent.firstName} {popupWindowContent.lastName}{" "}
+        </div>{" "}
         <div className="px-4 py-2">
-          <b>Email:</b> {popupWindowContent.email}
-        </div>
+          <b> Email: </b> {popupWindowContent.email}{" "}
+        </div>{" "}
         <div className="px-4 py-2">
-          <b>Phone Number:</b> {popupWindowContent.phoneNumber}
-        </div>
+          <b> Phone Number: </b> {popupWindowContent.phoneNumber}{" "}
+        </div>{" "}
         <div className="px-4 py-2">
-          <b>Role:</b> {popupWindowContent.role}
-        </div>
+          <b> Role: </b> {popupWindowContent.role}{" "}
+        </div>{" "}
         <div className="px-4 py-2">
-          <b>DOB:</b> {popupWindowContent.dateOfBirth}
-        </div>
+          <b> DOB: </b> {popupWindowContent.dateOfBirth}{" "}
+        </div>{" "}
         <div className="px-4 py-2">
-        <b>Address:</b> {popupWindowContent.addressStreet} {popupWindowContent.addressCity}, {popupWindowContent.addressState} {popupWindowContent.addressZipcode}
-        </div>
+          <b> Address: </b> {popupWindowContent.addressStreet}{" "}
+          {popupWindowContent.addressCity}, {popupWindowContent.addressState}{" "}
+          {popupWindowContent.addressZipcode}{" "}
+        </div>{" "}
         <div className="px-4 py-2">
-          <b>Last Login:</b> {popupWindowContent.lastLogin}
-        </div>
-        
-
-        
-        
-        
-        
+          <b> Last Login: </b> {popupWindowContent.lastLogin}{" "}
+        </div>{" "}
         {popupWindowContent.isLocked && (
-            <button
-                className="bottom-0 right-0 m-2 px-4 py-2 bg-blue-500 text-white rounded"
-                onClick={handleUnlockAccount}
-                >
-                Unlock Account
-            </button>
-        )}
-      </div>
+          <button
+            className="bottom-0 right-0 m-2 px-4 py-2 bg-blue-500 text-white rounded"
+            onClick={handleUnlockAccount}
+          >
+            Unlock Account{" "}
+          </button>
+        )}{" "}
+      </div>{" "}
     </div>
   );
 }
