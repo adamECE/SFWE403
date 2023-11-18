4d2255745c97ef65042cdca2468d93add685ff60
gatoAndres5 <andgalv@arizona.edu>
2023-11-17 19:15:25 -0700

    setup report page and directory

---
+++ pharmacy-manager-system/app/pages/dashboards/managerDashboard/inventoryReports/page.js
@@ -0,0 +1,197 @@
+"use client";
+import React, { useState } from "react";
+import AuthLogs from "../../../components/logs/auth";
+import InventoryLogs from "../../../components/logs/inventory";
+import PrescriptionLogs from "../../../components/logs/prescription";
+import PurchaseLogs from "@/pages/components/logs/purchase";
+
+export default function TabsDefault() {
+  const tabStyle = `block rounded border-x-0 border-b-2 mx-1 my-3 border-t-0 border-transparent bg-cyan-300 px-7 pb-3.5 pt-4 text-xs text-black font-medium uppercase leading-tight 
+    hover:isolate
+    hover:border-transparent
+    hover:bg-neutral-100
+    focus:isolate
+    focus:border-transparent
+    data-[te-nav-active]:border-primary
+    data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400`;
+  const [activeTab, setActiveTab] = useState("auth");
+  const [patientList, setPatientList] = useState();
+  const [prescriptioLogList, setPrescriptioLogList] = useState();
+    const [purchaseLogList, setPurchaseLogList] = useState();
+    const [authLogstList, setAuthLogstList] = useState([]);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+  };
+  const isDateRangeSelected = startDate !== null && endDate !== null;
+
+  return (
+    <>
+      <div className="flex border-b-0 mx-5">
+        <ul className="flex w-full" role="tablist" data-te-nav-ref>
+          <li role="presentation" className="flex-1">
+            <a
+              href="#auth"
+              className={` ${tabStyle} ${
+                activeTab === "auth"
+                  ? "text-primary bg-sky-100"
+                  : "text-neutral-500"
+              } `}
+              data-te-toggle="pill"
+              data-te-target="#auth"
+              data-te-nav-active
+              role="tab"
+              aria-controls="auth"
+              aria-selected={activeTab === "auth"}
+              onClick={() => handleTabClick("auth")}
+            >
+              Authentication
+            </a>
+          </li>
+          <li role="presentation" className="flex-1">
+            <a
+              href="#prescription"
+              className={` ${
+                activeTab === "prescription"
+                  ? "text-primary bg-sky-100"
+                  : "text-neutral-500"
+              } ${tabStyle}`}
+              data-te-toggle="pill"
+              data-te-target="#prescription"
+              role="tab"
+              aria-controls="prescription"
+              aria-selected={activeTab === "prescription"}
+              onClick={() => handleTabClick("prescription")}
+            >
+              Prescription filled
+            </a>
+          </li>
+          <li role="presentation" className="flex-1">
+            <a
+              href="#inventory"
+              className={` ${
+                activeTab === "inventory"
+                  ? "text-primary bg-sky-100"
+                  : "text-neutral-500"
+              } ${tabStyle}`}
+              data-te-toggle="pill"
+              data-te-target="#inventory"
+              role="tab"
+              aria-controls="inventory"
+              aria-selected={activeTab === "inventory"}
+              onClick={() => handleTabClick("inventory")}
+            >
+              Inventory update
+            </a>
+          </li>
+          <li role="presentation" className="flex-1">
+            <a
+              href="#purchase"
+              className={`disabled  ${
+                activeTab === "purchase"
+                  ? "text-primary bg-sky-100"
+                  : "text-neutral-400"
+              } ${tabStyle}`}
+              data-te-toggle="pill"
+              data-te-target="#purchase"
+              role="tab"
+              aria-controls="purchase"
+              aria-selected={activeTab === "purchase"}
+              onClick={() => handleTabClick("purchase")}
+            >
+              Purchases
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <label htmlFor="start-date">Start Date:</label>
+        <input
+          type="date"
+          id="start-date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        />
+        <label htmlFor="end-date">End Date:</label>
+        <input
+          type="date"
+          id="end-date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+        />
+      </div>
+      {isDateRangeSelected && (
+        <div className="mb-6 mx-5">
+          <div
+            className={`${
+              activeTab === "auth" ? "block " : "hide "
+            } transition-opacity duration-150 ease-linear data-[te-tab-active]:block`}
+            id="auth"
+            role="tabpanel"
+            aria-labelledby="auth-tab"
+            data-te-tab-active
+          >
+            <AuthLogs
+              authLogstList={authLogstList.filter((log) => {
+                const logDate = new Date(log.timestamp);
+                return (
+                  (!startDate || logDate >= new Date(startDate)) &&
+                  (!endDate || logDate <= new Date(endDate))
+                );
+              })}
+              setAuthLogstList={setAuthLogstList}
+            />
+        </div>
+        <div
+          className={`${
+            activeTab === "prescription"
+              ? "block"
+              : "hide "
+          } transition-opacity duration-150 ease-linear data-[te-tab-active]:block`}
+          id="prescription"
+          role="tabpanel"
+          aria-labelledby="prescription-tab"
+        >
+          <PrescriptionLogs
+            prescriptioLogList={prescriptioLogList}
+            setPrescriptioLogList={setPrescriptioLogList}
+          />
+        </div>
+        <div
+          className={`${
+            activeTab === "inventory"
+              ? "block"
+              : "hide "
+          } transition-opacity duration-150 ease-linear data-[te-tab-active]:block`}
+          id="inventory"
+          role="tabpanel"
+          aria-labelledby="prescription-tab"
+        >
+          <InventoryLogs
+            patientList={patientList}
+            setPatientList={setPatientList}
+          />
+        </div>
+        <div
+          className={`${
+            activeTab === "purchase"
+              ? "block"
+              : "hide "
+          } transition-opacity duration-150 ease-linear data-[te-tab-active]:block`}
+          id="purchase"
+          role="tabpanel"
+          aria-labelledby="purchase-tab"
+        >
+          <PurchaseLogs
+            purchaseLogList={purchaseLogList}
+            setPurchaseList={setPurchaseLogList}
+          />
+          
+        </div>
+      </div>
+      )}
+    </>
+  );
+}
+
